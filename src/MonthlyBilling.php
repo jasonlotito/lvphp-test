f353781c7f9a1de424ba6ffe9c0cc7dc0ae7a268
Jason Lotito <jasonlotito@gmail.com>
2012-11-24 19:25:06 -0500

    Added comment in MonthlyBilling

--- src/MonthlyBilling.php
+++ src/MonthlyBilling.php
@@ -5,6 +5,9 @@ class MonthlyBilling
   public function billUsers()
   {
     $cnt = 0;
+
+    // I don't like Users in Monthly Billing
+    // Let's remove that
     foreach( Users::getSubscribed() as $userStatus ) {
       Users::chargeUser($userStatus) ? $cnt++ : false;
     }
